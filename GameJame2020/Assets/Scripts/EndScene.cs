8759234043f38bcd599b61b13142bdd0cd2e7c34
Claire Taylor-Cuthbertson <30706470+Claire-Taylor-Cuthbertson@users.noreply.github.com>
2020-02-01 03:35:48 +1100

    End game scene
    Scene added for end of game, handle/toggle win or lose canvas depending on final state (yet to determine how we judge this/pass the value)

---
+++ GameJame2020/Assets/Scripts/EndScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScene : MonoBehaviour
+{
+    public GameObject win;
+    public GameObject lose;
+
+    void Awake()
+    {
+        //decide which canvas to display
+        /*if(win){
+        win.SetActive(true);
+        lose.SetActive(false);
+        }else{
+        win.SetActive(false);
+        lose.SetActive(true);
+        }
+    */
+    }
+
+    public void OnContinueClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
