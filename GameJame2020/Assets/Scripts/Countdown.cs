cba67f54f8c401eaf3fa6a0b57a6ae807b544b43
Claire Taylor-Cuthbertson <30706470+Claire-Taylor-Cuthbertson@users.noreply.github.com>
2020-02-01 06:15:21 +1100

    60 second countdown
    added timer top right corner, thinking of changing it to move of liike a clock fillig up or something more visual and nautical themed, but works for now, will add in win and lose state

---
+++ GameJame2020/Assets/Scripts/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public int timeLeft = 60;
+    public TextMeshProUGUI countdown;
+
+    void Start()
+    {
+        StartCoroutine(LoseTime());
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        countdown.text = (timeLeft.ToString());
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
