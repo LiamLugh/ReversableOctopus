343de01185149e151831772c2de46f10a85dca65
Claire Taylor-Cuthbertson <30706470+Claire-Taylor-Cuthbertson@users.noreply.github.com>
2020-02-01 03:20:25 +1100

    Drag + Drop
    Tested with dragable icon, when an icon is dropped on the cannon we get its name, it will then trigger a coroutine (yet to be made) that will animate that animal being shot from a cannon and delete the icon from the UI

---
+++ GameJame2020/Assets/Scripts/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour, IHasChanged
+{
+	[SerializeField] Transform slots;
+
+	// Use this for initialization
+	void Start()
+	{
+		HasChanged();
+	}
+
+	#region IHasChanged implementation
+	public void HasChanged()
+	{
+		System.Text.StringBuilder builder = new System.Text.StringBuilder();
+		foreach (Transform slotTransform in slots)
+		{
+			GameObject item = slotTransform.GetComponent<Slot>().item;
+			if (item)
+			{
+				builder.Append(item.name);
+			}
+		}
+		//inventoryText.text = builder.ToString();
+		//here we remove the dropped animal from the inventory(icon) and boot it off the ship via the cannon :D
+	}
+	#endregion
+}
+
+
+namespace UnityEngine.EventSystems
+{
+	public interface IHasChanged : IEventSystemHandler
+	{
+		void HasChanged();
+	}
+}
