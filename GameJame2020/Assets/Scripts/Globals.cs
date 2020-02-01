fd8697285d18d82cc8c46ad8e8603ac0af28bfaa
Claire Taylor-Cuthbertson <30706470+Claire-Taylor-Cuthbertson@users.noreply.github.com>
2020-02-02 00:46:22 +1100

    small changes

--- GameJame2020/Assets/Scripts/Globals.cs
+++ GameJame2020/Assets/Scripts/Globals.cs
@@ -2,7 +2,7 @@
 {
     public static bool musicVolume = true;
     public static bool paused = false;
-    public static int levelPairCount = 3;
+    public static int levelPairCount = 2;
     public static bool win = false;
     public static int time = 30;
 }
